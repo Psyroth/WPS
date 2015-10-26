81b30f30f3db48666f4860758a324e8e49677255
Philippe Veranneman <phvera@linux-b99h.site>
2015-10-26 16:47:23 +0100

    Added test account to list view of accounts to test it out

--- src/com/example/wps/gui/ListViewActivity.java
+++ src/com/example/wps/gui/ListViewActivity.java
@@ -5,12 +5,30 @@ import com.example.wps.R;
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.TextView;
+import com.example.wps.db.Account;
 
 public class ListViewActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        Account testAccount1 = new Account("Facebook",
+				"facebookUser@hotmail.com", "facebook",
+				"https://www.facebook.com", "Sat, 12 Aug 2005 13:30:00 GMT",
+				"Less useful than Linkedin", "Social Network");
+        
+        String userTitle = testAccount1.getName();
+        String userID = testAccount1.getId();
+        String userPass = testAccount1.getPassword();
+        
+        TextView tv = (TextView) findViewById(R.id.editText15);
+        tv.setText(userTitle+"\n"+userID+userPass);
+//        tv.setText(userPass);
+        
 		setContentView(R.layout.account_list_scrollview_layout);
+		
+		
+		
+		
 //        TextView textview = new TextView(this);
 //        textview.setText("This is the list view tab");
 //        setContentView(textview);
